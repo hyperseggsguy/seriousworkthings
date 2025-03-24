57a6e4cec8c48da3c34d8e41618b046e1e61ffb8
hyperseggsguy <timsonsdoughnuts@gmail.com>
2025-03-24 13:09:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('prankButton').addEventListener('click', function() {
+    alert("Jokes on u bud :)");
+    // Redirecting to a funny video (Rickroll)
+    window.location.href = "https://www.youtube.com/watch?v=WIRK_pGdIdA&list=PLpQpolEIRhPtKIte1nTmjYcJCriNwj20L";
+});
